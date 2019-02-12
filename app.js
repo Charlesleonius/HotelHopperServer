374d73533e34b11390ad139f627b04dffbda2cab
Charles Leon <charles.p.leon@gmail.com>
2019-02-11 17:35:04 -0800

    Add missing route

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/favicon.ico', (req, res) => res.send('Hello World!'))
