ef528c6f3e16fb731a3d94381d6510fb86e4c25e
Bruce Lin <brucelin1218@gmail.com>
2019-03-14 00:09:43 -0700

    added the endpoint for /forgotPassword in Auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,6 +85,67 @@ router.post('/login', function (req, res) {
 });
 
 /*
+* Handle when the registered user forgets the password and send the password reset link to the registered email
+*/
+router.post('/forgotPassword', function(req, res) {
+    async.waterfall([ // ensure each function is executed one after another
+        function(done) {
+            db.user.findOne({ where: { email: req.body.email }}).exec(function(err, user) {
+                if (user) {
+                    done(err, user)
+                } else {
+                    done("User not found")
+                }
+            });
+        },
+
+        function(user, done) {
+            // create the random token
+            crypto.randomBytes(20, function(err, buffer) {
+                var token = buffer.toString('hex');
+                done(err, user, token);
+            });
+        },
+
+        function(user, token, done) {
+            db.user.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function(err, new_user) {
+                done(err, token, new_user);
+            });
+        },
+
+        function(token, user, done) {
+            var data = {
+              to: db.user.email,
+              from: email,
+              template: 'forgot-password-email',
+              subject: 'Reset Your Passwork from Hotel Hopper',
+              context: {
+                url: 'http://localhost:3000/auth/reset_password?token=' + token,
+                name: db.user.fullName.split(' ')[0]
+              }
+            };
+
+            smtpTransport.sendMail(data, function(err) {
+                if (!err) {
+                  return res.json({ message: "Check your email for further instructions" });
+                } else {
+                  return done(err);
+                }
+            });
+        }
+    ], function(err) {
+        return res.status(422).json({ message: err });
+    })
+});
+
+/*
+* Reset the password
+*/
+router.post('/resetPassword', function(req, res) {
+    
+});
+
+/*
 * Route for testing auth functionality
 */
 router.get("/secret", passport.authenticate('jwt', { session: false }), function (req, res) {
