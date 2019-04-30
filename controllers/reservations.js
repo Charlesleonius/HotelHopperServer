07495ce4777cb977484599432cb34b5969afa9d3
Riley Chetwood <riley.chetwood@sjsu.edu>
2019-04-29 17:31:14 -0700

    cancel end point with /:id/:status

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const Validator = require('validatorjs');
-const { 
-    sendValidationErrors, requireAuth, 
-    sendErrorMessage, catchAll 
+const {
+    sendValidationErrors, requireAuth,
+    sendErrorMessage, catchAll
 } = require('../middleware.js');
 const { Reservation, ReservedRoom } = require('../models/index.js');
 const moment = require('moment')
@@ -17,25 +17,25 @@ var stripe = require("stripe")(process.env.STRIPE_SK);
  * @TODO - Add validation to ensure the proper amount of each room is available. 
  * Add stripe, rewards points, and a confirmation email.
  */
-router.post('/', requireAuth, async (req, res) =>{
+router.post('/', requireAuth, async (req, res) => {
     let validator = new Validator({
         hotelID: req.body.hotelID,
         rooms: req.body.rooms,
         startDate: req.body.startDate,
         endDate: req.body.endDate,
         stripeToken: req.body.stripeToken
-      }, {
-        hotelID: 'required|integer',
-        rooms: 'required',
-        'rooms.*.roomTypeID': 'required|integer',
-        'rooms.*.count': 'required|integer',
-        startDate: 'required|date|regex:/[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
-        endDate: 'required|date|regex:/[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
-        stripeToken: 'required|string'
-    },{
-        "regex.startDate": "Please use the date format yyyy-mm-dd",
-        "regex.endDate": "Please use the date format yyyy-mm-dd"
-    });
+    }, {
+            hotelID: 'required|integer',
+            rooms: 'required',
+            'rooms.*.roomTypeID': 'required|integer',
+            'rooms.*.count': 'required|integer',
+            startDate: 'required|date|regex:/[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+            endDate: 'required|date|regex:/[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+            stripeToken: 'required|string'
+        }, {
+            "regex.startDate": "Please use the date format yyyy-mm-dd",
+            "regex.endDate": "Please use the date format yyyy-mm-dd"
+        });
     if (validator.fails()) return sendValidationErrors(res, validator);
     let startDate = moment(req.body.startDate).format("YYYY-MM-DD");
     let endDate = moment(req.body.endDate).format('YYYY-MM-DD');
@@ -44,9 +44,9 @@ router.post('/', requireAuth, async (req, res) =>{
         return res.status(400).json({
             error: true,
             message: "Invalid date range. Make sure your reservation isn't in the past!"
-        }); 
+        });
     }
-    Reservation.query().insert({ 
+    Reservation.query().insert({
         hotelID: req.body.hotelID,
         userID: req.user.userId,
         startDate: startDate,
@@ -80,7 +80,7 @@ router.post('/', requireAuth, async (req, res) =>{
             } else if (err) {
                 console.log(err);
                 return sendErrorMessage(res, 500, "Could not complete charge. "
-                                                + "Please try again later.");
+                    + "Please try again later.");
             }
             return res.status(200).json({
                 error: false,
@@ -101,19 +101,21 @@ router.post('/', requireAuth, async (req, res) =>{
  * @Description - Updates the status of a given reservation
  * @TODO - Charge for cancellation, reward points?
  */
-router.patch('/:id/:status', async (req, res) => {
+router.patch('/:id/:status', requireAuth, async (req, res) => {
     let validator = new Validator({
         id: req.params.id,
-        status: req.params.status
-      }, {
-        id: 'required|numeric|min:1',
-        status: 'required|string'
-    });
+        status: req.params.status,
+        stripeToken: req.body.stripeToken
+    }, {
+            id: 'required|numeric|min:1',
+            status: 'required|string',
+            stripeToken: 'required|string'
+        });
     if (validator.fails()) return sendValidationErrors(res, validator);
     const id = req.params.id;
     const status = req.params.status;
 
-    if(!(status == 'pending' || status == 'completed' || status == 'cancelled')) {
+    if (!(status == 'pending' || status == 'completed' || status == 'cancelled')) {
         return res.status(404).json({
             error: true,
             message: 'Status can only be pending, completed, or cancelled'
@@ -121,22 +123,81 @@ router.patch('/:id/:status', async (req, res) => {
     }
 
     let reservation = await Reservation.query()
-                                        .select()
-                                        .from('reservation')
-                                        .where({reservation_id: id});
+        .select()
+        .from('reservation')
+        .where({ reservation_id: id }).findOne();
 
-    if(reservation.length == 0) {
+    if (reservation) {
         return res.status(404).json({
             error: true,
             message: `Reservation ID ${id} does not exist`
         });
+    } else if(reservation.status == 'cancelled') {
+        return res.status(401).json({
+            error: true,
+            message: `You're not allowed to change a cancelled reservation`
+        });
     }
 
-    await Reservation.query().where({reservation_id: id}).update({current_status: status});
-    return res.status(200).json({
-        error: false,
-        message: 'Your reservation has been updated'
-    });
+    await Reservation.query().where({ reservation_id: id }).update({ current_status: status });
+
+    if (status == 'cancelled') {
+
+        // refund
+        var [err, refund] = await catchAll(stripe.refunds.create({
+            charge: reservation.charge,
+        }));
+        if (err && err.statusCode == 400) {
+            return sendErrorMessage(res, 400, err.message);
+        } else if (err) {
+            console.log(err);
+            return sendErrorMessage(res, 500, "Could not complete refund. "
+                + "Please try again later.");
+        }
+
+        // cancellation fee
+        var [err, charge] = await catchAll(stripe.charges.create({
+            amount: 4500,
+            currency: 'usd',
+            description: 'Cancelled reservation id: ' + reservation.reservationId,
+            source: req.body.stripeToken,
+            customer: req.user.stripeCustomerId
+        }));
+        if (err && err.statusCode == 400) {
+            return sendErrorMessage(res, 400, err.message);
+        } else if (err) {
+            console.log(err);
+            return sendErrorMessage(res, 500, "Could not complete refund. "
+                + "Please try again later.");
+        }
+
+        const mailOptions = {
+            from: "hotelhopperhelp@gmail.com",
+            to: req.user.email,
+            subject: "Your reservation has been canceled",
+            text: "Your reservation has been successfully canceled! We are sorry that we have to charge you $45."
+        };
+        let emailTransporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+        emailTransporter.sendMail(mailOptions, function (err, response) {
+            if (err) console.log("error: ", err);
+        });
+        return res.status(200).json({
+            error: false,
+            message: "Your reservation has been successfully canceled! We are sorry that we have to charge you $45"
+        });
+
+    } else {
+        return res.status(200).json({
+            error: false,
+            message: `Your reservation has been updated to ${status}.`
+        });
+    }
 });
 
 module.exports = router;
